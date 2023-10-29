6afe1a512ea7d0966a261e2d1a736529e0b5cd10
CRUFTY <crufty01@gmail.com>
2023-10-29 13:24:08 -0300

    .
    .

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("myForm");
+    const submitButton = document.getElementById("submit");
+
+    submitButton.addEventListener("click", function() {
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const subject = document.getElementById("subject").value;
+        const body = document.getElementById("body").value;
+
+        // Realiza una solicitud AJAX para enviar los datos del formulario a mail.php
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "mail.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                // Redirige a una página de confirmación o muestra un mensaje en el mismo lugar
+                window.location = "confirmacion.html";
+            } else {
+                // Maneja errores
+                alert("Error al enviar el formulario.");
+            }
+        };
+
+        const data = `name=${name}&email=${email}&subject=${subject}&body=${body}`;
+        xhr.send(data);
+    });
+});
